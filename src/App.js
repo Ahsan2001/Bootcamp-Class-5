1ae0bccac70be0fdc46f1d73cd35de932357a488
Muhammad Ahsan <55804800+Ahsan2001@users.noreply.github.com>
2021-03-22 16:50:02 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,55 @@
+// three level of changing data
+
+//------------------------Level 2 Start -
+
 import React, { useState } from 'react'
 import Parent from './Components/parent';
+import myContext from './Components/context';
 
 function App() {
-
 let [number, setNumber] = useState(10)
+  return (
 
+    <myContext.Provider value={number}>
+      <Parent />
+      <button onClick={ () => {setNumber(++number)}}>Click me to add number</button>
+     </myContext.Provider>
+  );
+}
+export default App;
+//------------------------Level 2 End -
+
+
+
+
+
+
+
+
+
+//------------------------Level 1 Start -
+{/*
+
+import React, { useState } from 'react'
+import Parent from './Components/parent';
+function App() {
+let [number, setNumber] = useState(10)
   return (
      <div>
       <Parent myProps={number}/>
       <button onClick={ () => {setNumber(++number)}}>Click me to add number</button>
-      <h1>Voulme 1</h1>
      </div> 
   );
 }
-
 export default App;
+
+*/}
+
+// ------------------------- level 1 End
+
+
+
+
+
+
+
